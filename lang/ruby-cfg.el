ed37de3b793c925da46ebbca90d914f2a38fb29d
aguestuser <aguestuser@riseup.net>
2017-10-08 19:15:03 -0400

    update ruby config

--- lang/ruby-cfg.el
+++ lang/ruby-cfg.el
@@ -5,13 +5,22 @@
 (use-package ruby-mode :ensure t)
 (use-package rspec-mode :ensure t)
 (use-package robe :ensure t)
+(use-package rvm :ensure t)
 (use-package projectile-rails :ensure t)
+
+;; ensure rvm loads correct ruby version
+(defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+  (rvm-activate-corresponding-ruby))
 
 ;; enable projectile-rails
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
+(add-hook 'projectile-mode-hook 'projectile-rails-mode)
+(add-hook 'ruby-mode-hook 'projectile-rails-on)
+(add-hook 'ruby-mode-hook 'projectile-rails-mode)
 
 ;; enable robe-mode
 (add-hook 'ruby-mode-hook 'robe-mode) ; add hook
+
 (eval-after-load 'company
   '(push 'company-robe company-backends)) ; company mode
 (add-hook 'robe-mode-hook 'ac-robe-setup) ; auto-complete
@@ -33,3 +42,31 @@
 
 ;; disable expand-snippet class skeletons
 (setq projectile-rails-expand-snippet nil)
+
+;; hide/show blocks
+(add-hook 'ruby-mode-hook
+          (lambda () (hs-minor-mode)))
+(eval-after-load "hideshow"
+  '(add-to-list 'hs-special-modes-alist
+                `(ruby-mode
+                  ,(rx (or "def" "class" "module" "describe" "do" "{" "[")) ; Block start
+                  ,(rx (or "}" "]" "end"))           ; Block end
+                  ,(rx (or "#" "=begin"))            ; Comment start
+                  ruby-forward-sexp nil)))
+;; (global-set-key (kbd "C-c h") 'hs-hide-block)
+;; (global-set-key (kbd "C-c s") 'hs-show-block)
+
+;; eruby stuff
+
+;; (sp-with-modes '(web-mode)                                                                                                           
+;;   (sp-local-pair "%" "%"                                                                                                   :unless '(sp-in-string-p)          
+;;                  :post-handlers '(((lambda (&rest _ignored)                    
+;;                                      (just-one-space)                          
+;;                                      (save-excursion (insert " ")))            
+;;                                    "SPC" "=" "#"))) 
+;;   (sp-local-pair "<% "  " %>" :insert "C-c %")      
+;;   (sp-local-pair "<%= " " %>" :insert "C-c =")      
+;;   (sp-local-pair "<%# " " %>" :insert "C-c #")      
+;;   (sp-local-tag "%" "<% "  " %>")                   
+;;   (sp-local-tag "=" "<%= " " %>")                   
+;;   (sp-local-tag "#" "<%# " " %>"))
