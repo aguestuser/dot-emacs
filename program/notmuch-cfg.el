0a83d6fd39374b4141ebffce43947c4b360634f7
aguestuser <aguestuser@riseup.net>
2017-10-08 19:15:29 -0400

    add provisional (commented) notmuch config

--- program/notmuch-cfg.el
+++ program/notmuch-cfg.el
@@ -7,16 +7,28 @@
 
 ;; (use-package notmuch-crypto-process-mime :ensure t)
 ;; (require 'notmuch-crypto-process-mime)
+
+;; decrypt messages
 (setq notmuch-crypto-process-mime t)
 
-;; (use-package smptmail :ensure t)
-;; (use-package starttls :ensure t)
+;; sign and encrypte messages by default
+(add-hook 'message-setup-hook 'mml-secure-message-encrypt-pgpmime)
+;;(add-hook 'message-setup-hook 'mml-secure-message-sign-pgpmime)
 
+
+;; auto-complete w/ helm
+(setq notmuch-address-selection-function
+      (lambda (prompt collection initial-input)
+        (completing-read prompt (cons initial-input collection) nil t nil 'notmuch-address-history)))
+
+;; (use-package smptmail :ensure t)
 ;; (require 'smtpmail)
-;; (require 'starttls)
+
+;; ;; (use-package starttls :ensure t)
+;; ;; (require 'starttls)
 
 ;; (setq smtpmail-starttls-credentials '(("riseup.net" 587 nil nil))
-;;       smtpmail-auth-credentials (expand-file-name "~/.pw/agriseup")
+;;       ;; smtpmail-auth-credentials (expand-file-name "~/.passwordstoare/accounts/riseup/austin.guest")
 ;;       smtpmail-default-smtp-server "riseup.net"
 ;;       smtpmail-smtp-server "riseup.net"
 ;;       smtpmail-smtp-service 587)
