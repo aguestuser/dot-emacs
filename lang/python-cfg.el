eac6eaa3cc2f4cd0bc8005cf4bc515aa045e0301
aguestuser <aguestuser@riseup.net>
2017-10-16 15:06:40 -0400

    tweak python config (mainly formatting)

--- lang/python-cfg.el
+++ lang/python-cfg.el
@@ -5,6 +5,12 @@
 ;;;;;;;;;;;;
 
 
+;; IPYTHON MULTILINE GOTCHA:
+;; https://emacs.stackexchange.com/questions/14082/multi-line-inputs-in-ipython
+;; $ sudo pip install rlipython
+;; $ ipython
+;; [1] import rlipython; rlipython.install()
+
 ;;;;;;;;;;
 ;; elpy ;;
 ;;;;;;;;;;
@@ -12,26 +18,35 @@
 ;; PYTHON REQUIREMENTS: $ pip3 install flake8 jedi importmagic autopep8
 ;; DOCS: https://elpy.readthedocs.io/en/latest/
 
-(use-package elpy :ensure t)
+(use-package elpy
+  :ensure t
+  :init (progn
+          ;; (package-initialize)
+          (require 'elpy))
+  :config (progn
+            (elpy-enable)
+            (elpy-use-ipython)
+            (setq python-shell-interpreter "ipython3")
+            (setq python-shell-interpreter-args "--simple-prompt -i")
+            (setq elpy-rpc-python-command "python3")
+            (add-hook 'elpy-mode-hook
+                      '(lambda ()
+                         (local-set-key (kbd "C-c , t") 'elpy-test-nose-runner)))))
 
-(elpy-enable)
-(elpy-use-ipython)
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "--simple-prompt -i")
-
-(setq elpy-rpc-python-command "python3")
-
-;; set hook for setting highlight indentation color
+;; set hook for setting highlight indentation colo
 (add-hook 'python-mode-hook (highlight-indentation-mode 1))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Jedi (code completion, etc.) ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package jedi :ensure t)
-;; GOTCHA: on first run might require: `M-x jed:install-server`
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
+(use-package jedi
+  :ensure t
+  :config (progn
+            ;; GOTCHA: on first run might require: `M-x jed:install-server`
+            (add-hook 'python-mode-hook 'jedi:setup)
+            (setq elpy-rpc-backend "jedi")
+            (setq jedi:complete-on-dot t)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Emacs IPython Notebook (EIN) ;;
@@ -54,14 +69,14 @@
   :config (progn
             ;; use `url-retrieve` instead of `curl`
             ;; as per: https://github.com/tkf/emacs-ipython-notebook/issues/130
-            (setq request-backend 'url-retrieve)
+            ;; (setq request-backend 'url-retrieve)
             ;; make jupyter startup from emacs easier (note: actually running from emacs borked for me)
             ;; as per: https://github.com/millejoh/emacs-ipython-notebook/issues/176
             (setq ein:jupyter-default-server-command "/usr/local/bin/jupyter")
             (setq ein:jupyter-server-args (list "--no-browser"))
             (setq ein:completion-backend 'ein:use-ac-jedi-backend)))
 
-;; AUTHENTICATION GOTCHA:
+;; EIN AUTHENTICATION GOTCHA:
 ;; https://github.com/millejoh/emacs-ipython-notebook/issues/147
 
 ;; USAGE:
@@ -75,4 +90,3 @@
 ;;     M-x ein:notebooklist-open
 ;; ..OR..
 ;; (2) copy and past login token from terminal every time you start
-
